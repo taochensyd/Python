d71aedb0a660a59320bdf583db6adadb11a7da98
Tao Chen <54517105+taochensyd@users.noreply.github.com>
2022-04-18 11:20:04 +1000

    fix error

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,40 +1,49 @@
 import random
 from words import words
+import string
 
 
 def get_valid_word(words):
     word = random.choice(words)
     while '-' in word or ' ' in word:
         word = random.choice(words)
-    return word
+    return word.upper()
 
 
 def hangman():
-    word = get_valid_word(word)
+    word = get_valid_word(words)
     word_letters = set(word)
     alphabet = set(string.ascii_uppercase)
     used_letters = set()
 
+    lives = 7
+
     # get user input
-    while len(word_letters) > 0:
+    while len(word_letters) > 0 and lives > 0:
         # letters used
         print('Letters used:', ' '.join(used_letters))
         # current word state
         word_list = [
             letter if letter in used_letters else '-' for letter in word]
-        print('current word:', ' '.join(word_list))
+        print('Current word:', ' '.join(word_list))
 
-        user_letters = input("guess a letter: ").upper()
-        if user_letters in alphabet:
-            used_letters.add(user_letters)
-            if user_letters in word_letters:
-                word_letters.remove(user_letters)
-
-        elif user_letters in used_letters:
+        user_letter = input("Guess a letter: ").upper()
+        if user_letter in alphabet - used_letters:
+            used_letters.add(user_letter)
+            if user_letter in word_letters:
+                word_letters.remove(user_letter)
+            else:
+                lives = lives - 1  # wrong guess - 1 live
+        elif user_letter in used_letters:
             print("You have already used that character. Please try again.")
         else:
             print("Please enter a valid letter.")
 
+    if lives == 0:
+        print("You lost. The word was", word)
+    else:
+        print("You won! The word was", word)
 
-user_input = input("Guess a letter: ")
-print(user_input)
+
+if __name__ == '__main__':
+    hangman()
